3c6763df3cc9c7019165ecd5d6e6880d2335ad6a
erictengland <wuchengbuo@gmail.com>
2015-06-07 16:31:28 -0700

    Fixed a data error

--- plot1.r
+++ plot1.r
@@ -1,22 +1,22 @@
-# Data downloading and processing
+# Data downloading and processing.
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data <- read.csv(unz(temp, "household_power_consumption.txt"),sep=";")
 unlink(temp)
 rm(temp)
-subset1<-data[data$Date=="2/1/2007",]
+subset1<-data[data$Date=="1/2/2007",]
 subset2<-data[data$Date=="2/2/2007",]
 subset<-rbind(subset1,subset2)
 rm(data)
 texttonum <- function(x){return(as.double(as.character(x)))}
 subset[,3:9]<-lapply(subset[,3:9], texttonum)
-texttodate <-function(x){return(as.Date(x,"%m/%d/%Y"))}
+texttodate <-function(x){return(as.Date(x,"%d/%m/%Y"))}
 subset$PosixDate<-sapply(subset[,1],texttodate)
 x<-paste(subset[,1],subset[,2])
 datetime<-list()
 for (i in 1:2880)
 {
-  datetime[[i]]<-(strptime(x[i], "%m/%d/%Y %H:%M:%S"))
+  datetime[[i]]<-(strptime(x[i], "%d/%m/%Y %H:%M:%S"))
 }
 # End of data processing
 #Begin actual graphics work
--- plot3.r
+++ plot3.r
@@ -1,28 +1,28 @@
-# Data downloading and processing
+# Data downloading and processing. 
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data <- read.csv(unz(temp, "household_power_consumption.txt"),sep=";")
 unlink(temp)
 rm(temp)
-subset1<-data[data$Date=="2/1/2007",]
+subset1<-data[data$Date=="1/2/2007",]
 subset2<-data[data$Date=="2/2/2007",]
 subset<-rbind(subset1,subset2)
 rm(data)
 texttonum <- function(x){return(as.double(as.character(x)))}
 subset[,3:9]<-lapply(subset[,3:9], texttonum)
-texttodate <-function(x){return(as.Date(x,"%m/%d/%Y"))}
+texttodate <-function(x){return(as.Date(x,"%d/%m/%Y"))}
 subset$PosixDate<-sapply(subset[,1],texttodate)
 x<-paste(subset[,1],subset[,2])
 datetime<-as.POSIXlt(character())
 for (i in 1:2880)
 {
-  datetime[i]<-as.POSIXlt(strptime(x[i], "%m/%d/%Y %H:%M:%S"))
+  datetime[i]<-as.POSIXlt(strptime(x[i], "%d/%m/%Y %H:%M:%S"))
 }
 # End of data processing
 #Begin actual graphics work
 png(filename="plot3.png")
 plot(datetime,subset$Sub_metering_3,type="o",pch="",xlab="",
-     col="blue", ylab="Energy Sub Metering",ylim=c(0,30))
+     col="blue", ylab="Energy Sub Metering",ylim=c(0,40))
 points(datetime,subset$Sub_metering_2,type="o",pch="",col="red")
 points(datetime,subset$Sub_metering_1,type="o",pch="")
 legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8, 
